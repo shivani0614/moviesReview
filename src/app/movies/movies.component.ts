cc7f6e0a5eccc3330efd10773d82ce29c0ea5bf9
shivani0614 <shivanialeti14@gmail.com>
2024-01-04 00:31:19 -0700

    navigation through reviews

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrl: './movies.component.scss'
+  styleUrls: ['./movies.component.scss'],
 })
-export class MoviesComponent {
+export class MoviesComponent implements OnInit {
+[x: string]: any;
+  type = '';
+  id = '';
+  url = '';
+  movies: any;
+  movie: any;
 
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.type = this.route.snapshot.params['type'];
+    this.id = this.route.snapshot.params['id'];
+    if (this.type === 'trending') {
+      this.url = 'http://localhost:4200/assets/data/trending-movies.json';
+    }
+    if (this.type === 'theatre') {
+      this.url = 'http://localhost:4200/assets/data/theatre-movies.json';
+    }
+    if (this.type === 'popular') {
+      this.url = 'http://localhost:4200/assets/data/popular-movies.json';
+    }
+    this.getMovie();
+  }
+
+  getMovie() {
+    this.http.get(this.url).subscribe((movies) => {
+      this.movies = movies;
+      let index = this.movies.findIndex(
+        (movie: { id: string }) => movie.id == this.id
+      );
+      if (index > -1) {
+        this.movie = this.movies[index];
+      }
+    });
+  }
 }
